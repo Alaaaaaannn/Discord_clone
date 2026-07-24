9ee975545a92adffacda07b2ec2573b83c5773f8
Alan Fernandes <alanferns2023@gmail.com>
2026-07-24 23:18:17 +0530

    fix: Server sidebar image render issue and aspect ratio

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -36,7 +36,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
         {isModerator && (
-          <DropdownMenuItem onClick={() => onOpen("invite",{server})} className="text-indigo-400 px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("invite", { server })}
+            className="text-indigo-400 px-3 py-2 text-sm cursor-pointer"
+          >
             Invite People
             <UserPlus className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
@@ -66,12 +69,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
             <TrashIcon className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
         )}
-        {isAdmin && (
-          <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
-            Leave Server
-            <LogOut className="h-4 w-4 ml-auto" />
-          </DropdownMenuItem>
-        )}
+        <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+          Leave Server
+          <LogOut className="h-4 w-4 ml-auto" />
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
