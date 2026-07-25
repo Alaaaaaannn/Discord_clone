fc357603c715b69663e45e80810f1d14e1b1c7c2
Alan Fernandes <alanferns2023@gmail.com>
2026-07-25 14:27:03 +0530

    added server routing to channels on members on search bar click

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -9,7 +9,8 @@ import {
   CommandList,
 } from "@/components/ui/command";
 import { Search } from "lucide-react";
-import { useState } from "react";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 interface ServerSearchProps {
   data: {
@@ -27,6 +28,28 @@ interface ServerSearchProps {
 
 export const ServerSearch = ({ data }: ServerSearchProps) => {
   const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const params = useParams();
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
+  const onClick = ({id, type}: {id:string, type:"channel" | "member"}) => {
+    setOpen(false);
+    if(type === "member") {
+        return router.push(`/servers/${params?.serverId}/conversations/${id}`)
+    }
+    if(type === "channel"){
+        return router.push(`servers/${params?.serverId}/channels/${id}`)
+    }
+  }
 
   return (
     <>
@@ -46,13 +69,13 @@ export const ServerSearch = ({ data }: ServerSearchProps) => {
         <CommandInput placeholder="Search all channels and members" />
         <CommandList>
           <CommandEmpty>No Results found</CommandEmpty>
-          {data.map(({ label, data }) => {
+          {data.map(({ label, type, data }) => {
             if (!data?.length) return null;
 
             return (
               <CommandGroup key={label} heading={label}>
                 {data.map(({ id, name, icon }) => (
-                  <CommandItem key={id} onSelect={() => setOpen(false)}>
+                  <CommandItem key={id} onSelect={() => onClick({id, type})}>
                     {icon}
                     <span>{name}</span>
                   </CommandItem>
