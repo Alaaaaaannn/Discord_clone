54afb58ed6fc4f790a5c47e853499ce279350a8f
Alan Fernandes <alanferns2023@gmail.com>
2026-07-25 13:33:17 +0530

    feat: leave server functionality added and bug fixed when joining a server with an invite code

---
+++ app/api/servers/[serverId]/leave/route.ts
@@ -0,0 +1,42 @@
+import { currentProfile } from "@/lib/current-profile";
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: Promise<{ serverId: string }> },
+) {
+  try {
+    const profile = await currentProfile();
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    if (!params) {
+      return new NextResponse("Server ID Missing", { status: 400 });
+    }
+    const server = await prisma.server.update({
+      where: {
+        id: (await params).serverId,
+        profileId: {
+          not: profile.id,
+        },
+        members: {
+          some: {
+            profileId: profile.id,
+          },
+        },
+      },
+      data: {
+        members: {
+          deleteMany: {
+            profileId: profile.id,
+          },
+        },
+      },
+    });
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVER_ID_LEAVE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
